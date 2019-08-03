2130edd2855663435baaf01627114fcfed7948b9
SebaAlex12 <sebastiank24@wp.pl>
2019-08-03 11:38:54 +0200

    first commit

---
+++ src/components/gallery/ImagesList.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class ImagesList extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Where is my gallery ???</h1>
+      </div>
+    );
+  }
+}
+export default ImagesList;
